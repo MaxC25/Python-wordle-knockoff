cb360b6f734d59016b5a24ff65857c33a596e12c
MaxC25 <7951@balcarras.gloucs.sch.uk>
2024-09-10 12:00:05 +0100

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,7 +1,9 @@
+import os
 from secrets import randbelow
 from linecache import getline,clearcache
 from sys import exit
-w=getline(r"WORDS.txt", randbelow(3104)).rstrip("\n")
+os.system("")
+w=getline(r"WORDS.txt", randbelow(3103)).rstrip("\n")
 clearcache()
 del randbelow,getline,clearcache
 for A in range(5):
@@ -13,6 +15,7 @@ for A in range(5):
   else:
     for B in range(0,5):
       if i[B]==w[B]:cor=cor+w[B]
-      elif i[B] in w:WP=WP+w[B]
-      else:WP,xcor=WP+" ",xcor+i[B]
-print('\n\033[102m'+cor+'\n\033[0m'+WP+'\n\033[101m]'+xcor+'\033[0m')
+      elif i[B] in w:WP=WP+i[B]
+      else:cor,WP,xcor=cor+' ',WP+' ',xcor+i[B]
+  print('\n\033[92m'+cor+'\033[0m\n'+WP+'\n\033[91m'+xcor+'\033[0m')
+print(w+"failed")
